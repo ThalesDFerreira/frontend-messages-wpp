5de4a577824e6fc81b8d3a4eecb0e3b760d56d03
ThalesDFerreira <thalesferreira190@gmail.com>
2023-06-17 13:56:19 -0300

    conofiguração tailwind css

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,17 +3,27 @@ import '../styles/components/Footer.css';
 
 const Footer = () => {
   const empresa = {
-    nome: 'Minha Empresa',
-    email: 'contato@minhaempresa.com',
-    telefone: '(00) 1234-5678',
+    nome: 'Ferreira Empreendimentos',
+    email: 'ufrepresentacoes.org@gmail.com',
+    telefone: '(16) 992746725',
   };
 
   return (
-    <footer className=''>
-      <div className=''>
+    <footer className='bg-red-600 text-white text-center py-6'>
+      <div className='container mx-auto flex justify-end'>
         <h3 className=''>{empresa.nome}</h3>
         <p className=''>Email: {empresa.email}</p>
-        <p className=''>Telefone: {empresa.telefone}</p>
+        <p className='text-white'>
+          WhatsApp:{' '}
+          <a
+            className=''
+            href='https://api.whatsapp.com/send?phone=5516992746725'
+            target='_blank'
+            rel='noopener noreferrer'
+          >
+            {empresa.telefone}
+          </a>
+        </p>
       </div>
     </footer>
   );
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import MyProvider from './context/MyProvider';
 import App from './App';
+import './index.css';
 
 ReactDOM.render(
   <React.StrictMode>
